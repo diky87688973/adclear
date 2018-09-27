96015e9791bf46ad69e14821226400293cc19091
diky87688973 <diky87@qq.com>
2018-09-27 20:59:11 +0800

    update

--- adclear.1.3.js
+++ adclear.1.3.js
@@ -11,6 +11,8 @@
  * 
  * 全局属性:
  * _showADClearLog = true; // 增加全局配置参数可输出更多日志
+ * 浏览器标签:
+ * javascript:(function(){var a=new XMLHttpRequest();a.open('GET','//raw.githubusercontent.com/diky87688973/adclear/11ad4aa20f9cbefc4cbe767a9232d9e2b259ca5b/adclear.1.3.js',true);a.onreadystatechange=function(){if(this.readyState==4&&(this.status>=200&&this.status<300||this.status===304||this.status===0||this.status===1223)){eval(this.responseText);}};a.send(null);})();
  */
 
 ( function() {
