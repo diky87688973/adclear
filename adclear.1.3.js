68bc715da18f013d262ad1f42201345189c715a7
diky87688973 <diky87@qq.com>
2018-10-09 21:51:23 +0800

    update

--- adclear.1.3.js
+++ adclear.1.3.js
@@ -1,7 +1,8 @@
 /**
  * Browser AdClear Plugin 1.3
- * 广告清理-PC版 v1.3
- * 
+ * https://github.com/diky87688973/adclear
+ *
+ * 广告清理-PC版
  * v1.3 增加属性规则
  * v1.2 清理内容广告
  * v1.1 清理浮动广告
@@ -9,10 +10,10 @@
  * @author Fan
  * @lastModify 2018-09-27
  * 
- * 全局属性:
+ * 全局属性：
  * _showADClearLog = true; // 增加全局配置参数可输出更多日志
- * 浏览器标签:
- * javascript:(function(){var a=new XMLHttpRequest();a.open('GET','//raw.githubusercontent.com/diky87688973/adclear/11ad4aa20f9cbefc4cbe767a9232d9e2b259ca5b/adclear.1.3.js',true);a.onreadystatechange=function(){if(this.readyState==4&&(this.status>=200&&this.status<300||this.status===304||this.status===0||this.status===1223)){eval(this.responseText);}};a.send(null);})();
+ * 浏览器标签：
+ * javascript:(function(){var t=new Date().getTime(),a=new XMLHttpRequest();a.open('GET','//raw.githubusercontent.com/diky87688973/adclear/11ad4aa20f9cbefc4cbe767a9232d9e2b259ca5b/adclear.1.3.js',true);a.onreadystatechange=function(){if(this.readyState==4&&(this.status>=200&&this.status<300||this.status===304||this.status===0||this.status===1223)){window.console&&console.log('Loading AdClear Plugin...\tused '+(new Date().getTime()-t)+'ms');eval(this.responseText);}};a.send(null);})();
  */
 
 ( function() {
